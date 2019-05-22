a1254516fa2eae056e4b3d21e0c1f6dd10110963
freixedelo <rui.freixedelo@xgo.pt>
2019-05-22 18:41:04 +0100

    starting dev w/ navigation react tutorial

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Alert, StyleSheet, Text, View, FlatList } from 'react-native';
+import { Button } from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigate('Profile', {name: 'Jane'})}
+      />
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const {navigate} = this.props.navigation;
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
+      </View>
+    );
+  }
+}
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: ProfileScreen},
+});
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# react-native-ipb
+# React Native Tutorial App
+React Native App using expo and experiencing some components and props.
