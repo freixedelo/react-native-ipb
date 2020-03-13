37b770e15de51b0c1594509b3adfbd56d5c5fbe8
freixedelo <rui.freixedelo@xgo.pt>
2020-03-13 18:39:01 +0000

    Styling, some logic working

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
-import {
-  Alert,
-  Button,
-  Text,
-  TextInput,
-  View,
-  ScrollView,
-  StyleSheet
-} from "react-native";
-import Constants from "expo-constants";
+import { Alert, Button, Text, TextInput, View, StyleSheet } from "react-native";
+// import Constants from "expo-constants";
 
 class HomeScreen extends Component {
   constructor() {
     super();
     this.state = {
       isEnable: true,
-      inputText: "",
+      inputText: "Change this text",
       inputTempText: "",
       buttonTitle: "Disable"
     };
@@ -25,27 +17,35 @@ class HomeScreen extends Component {
     const { isEnable } = this.state;
     this.setState({
       isEnable: !isEnable,
-      buttonTitle: isEnable ? "Disable" : "Enable"
+      buttonTitle: !isEnable ? "Disable" : "Enable"
     });
   };
 
   updateText = () => {
+    const { inputTempText } = this.state;
     this.setState({
-      inputText: this.state.inputTempText
+      inputText: inputTempText
+    });
+  };
+
+  updateTempText = text => {
+    this.setState({
+      inputTempText: text
     });
   };
 
   openAlert = () => {
+    const { buttonTitle, isEnable, inputTempText } = this.state;
     Alert.alert(
-      "Alert Title",
-      "My Alert Msg", //TODO change inputText
+      "Update text?",
+      inputTempText,
       [
         {
           text: "Cancel",
           onPress: () => console.log("Cancel Pressed"),
           style: "cancel"
         },
-        { text: "OK", onPress: () => console.log("OK Pressed") }
+        { text: "OK", onPress: this.updateText }
       ],
       { cancelable: false }
     );
@@ -53,30 +53,44 @@ class HomeScreen extends Component {
 
   render() {
     const { navigate } = this.props.navigation;
-    const { buttonTitle, isEnable } = this.state;
+    const { buttonTitle, isEnable, inputText, inputTempText } = this.state;
     return (
-      <ScrollView>
+      <View style={styles.container}>
         <Text style={styles.header}>Tutorial APP</Text>
-        <Button
-          title="Check list"
-          onPress={() => navigate("Listsc", { name: "unknown" })}
-        />
-
         <View>
-          <Text style={styles.title}>
-            This layout strategy lets the title define the width of the button.
-          </Text>
+          <Text style={styles.title}>{inputText}</Text>
+          <TextInput
+            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+            onChangeText={text => this.updateTempText(text)}
+            placeholder={"Add new text here"}
+            editable={isEnable}
+            value={inputTempText}
+          />
           <View style={styles.fixToText}>
-            <Button title={buttonTitle} onPress={this.updateState} />
-
+            <Button
+              title={buttonTitle}
+              onPress={this.updateState}
+              style={styles.button}
+            />
             <Button
               title="Update Text"
-              disabled={!isEnable}
+              disabled={!isEnable || inputTempText === ""}
               onPress={this.openAlert}
+              style={styles.button}
             />
           </View>
         </View>
-      </ScrollView>
+        <>
+          <Text style={styles.title}>
+            This button navigates to another page.
+          </Text>
+          <Button
+            title="Check list"
+            onPress={() => navigate("Listsc", { name: "unknown" })}
+            style={styles.button}
+          />
+        </>
+      </View>
     );
   }
 }
@@ -84,16 +98,26 @@ class HomeScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
-    marginHorizontal: 16,
-    alignItems: "center",
-    justifyContent: "center"
+    // marginTop: Constants.statusBarHeight,
+    marginHorizontal: 20
+  },
+  header: {
+    textAlign: "center",
+    marginVertical: 10,
+    fontSize: 20,
+    textAlign: "center"
+  },
+  button: {
+    maxWidth: 15,
+    backgroundColor: "#212121"
   },
   title: {
     textAlign: "center",
     marginVertical: 8
   },
   fixToText: {
+    marginVertical: 10,
+    alignItems: "center",
     flexDirection: "row",
     justifyContent: "space-between"
   }
