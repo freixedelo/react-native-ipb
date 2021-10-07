5f97f9947ab5f655c2d3ffc93979acd67e074ba3
freixedelo <rui.freixedelo@xgo.pt>
2021-10-07 14:48:05 +0100

    creating a new project from scratch

--- App.js
+++ App.js
@@ -1,41 +1,21 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import HomeScreen from "./screens/Homescreen.js";
-import DetailScreen from "./screens/Detailscreen.js";
-import ListScreen from "./screens/Listscreen.js";
-
-const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-
-function List() {
+export default function App() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="Listsc"
-        component={ListScreen}
-        options={{ title: "List Items" }}
-      />
-      <Stack.Screen name="Detail" component={DetailScreen} />
-    </Stack.Navigator>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" drawerType="slide">
-        <Drawer.Screen
-          name="Home"
-          component={HomeScreen}
-          // options={{ title: "Homepage", gestureEnabled: false }}
-        />
-        <Drawer.Screen name="List" component={List} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
