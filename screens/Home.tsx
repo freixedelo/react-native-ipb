9afffe144299d1122efb660413a50ab7c5f98dcf
freixedelo <rui.freixedelo@xgo.pt>
2021-11-30 20:23:14 +0000

    imagePicker

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Image, Text, View, Platform } from "react-native";
 import { Button } from "react-native-elements";
+import * as ImagePicker from "expo-image-picker";
 
 type Props = {
   navigation: any;
@@ -11,6 +12,34 @@ export function Home(props: Props) {
   const { navigation } = props;
 
   const [xpto, setXpto] = useState<any>(null);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -23,6 +52,10 @@ export function Home(props: Props) {
         title="Move to camera"
         onPress={() => navigation.navigate("Camera")}
       />
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 }
